65e8d29d63e5629e5f99812e69cb9b9108055c46
cui <devcui@outlook.com>
2022-09-23 09:46:32 +0800

    feat(highcharts): impl pyramid

--- highcharts-pyramid-chart.js
+++ highcharts-pyramid-chart.js
@@ -2,7 +2,7 @@
  * @Author: cui<devcui@outlook.com>
  * @LastEditors: cui<devcui@outlook.com>
  * @Date: 2022-09-22 17:20:31
- * @LastEditTime: 2022-09-22 17:26:45
+ * @LastEditTime: 2022-09-23 09:45:17
  * @FilePath: \custom-chart-plugins\highcharts-pyramid-chart.js
  * @Description: 
  * 
@@ -16,7 +16,18 @@ function HighChartsPyramidChart({ dHelper }) {
                 { label: "dimension", key: "dimension", type: "group", allowSameField: false },
                 { label: "metrics", key: "metrics", type: "aggregate", allowSameField: false }
             ],
-            styles: [],
+            styles: [
+                {
+                    label: '标题',
+                    key: 'title',
+                    comType: 'input',
+                },
+                {
+                    label: '描述',
+                    key: 'desc',
+                    comType: 'input',
+                }
+            ],
             i18ns: [],
         },
         isISOContainer: 'highcharts-pyramid',
@@ -40,7 +51,6 @@ function HighChartsPyramidChart({ dHelper }) {
         },
         onMount(options, context) {
             if (!options.containerId || !context.document) return;
-            if (!options.containerId || !context.document) return;
             const { window: { Highcharts } } = context
             const { containerId } = options
             instance = Highcharts.chart(containerId, {
@@ -49,7 +59,7 @@ function HighChartsPyramidChart({ dHelper }) {
                     marginRight: 100
                 },
                 title: {
-                    text: '销售金字塔',
+                    text: 'Title',
                     x: -50
                 },
                 plotOptions: {
@@ -65,19 +75,77 @@ function HighChartsPyramidChart({ dHelper }) {
                 legend: {
                     enabled: false
                 },
-                series: [{
-                    name: '用户',
-                    data: [
-                        ['访问网站', 15654],
-                        ['下载产品', 4064],
-                        ['询价', 1987],
-                        ['发送合同', 976],
-                        ['成交', 846]
-                    ]
-                }]
+                series: [
+                    {
+                        name: 'Series Title',
+                        data: [
+                            ['data1', 15654],
+                            ['data2', 4064],
+                            ['data3', 1987],
+                            ['data4', 976],
+                            ['data5', 846]
+                        ]
+                    }]
             });
         },
-        onUpdated(options, context) { },
+        onUpdated(options, context) {
+            if (!options.containerId || !context.document) return;
+            const { window: { Highcharts } } = context
+            const { config, dataset, containerId } = options;
+            const dataConfigs = config.datas || [];
+            const styleConfigs = config.styles;
+            const groupConfigs = dataConfigs
+                .filter(c => c.type === 'group')
+                .flatMap(config => config.rows || []);
+            const aggregateConfigs = dataConfigs
+                .filter(c => c.type === 'aggregate')
+                .flatMap(config => config.rows || []);
+
+            const objDataColumns = dHelper.transformToObjectArray(
+                dataset.rows,
+                dataset.columns,
+            );
+
+            let desc = styleConfigs.filter((style) => style.key === 'desc')
+            desc = desc.length > 0 ? desc[0].value : 'desc'
+
+            let title = styleConfigs.filter((style) => style.key === 'title')
+            title = title.length > 0 ? title[0].value : 'title'
+
+            const data = objDataColumns.map(dc => {
+                return [dc[dHelper.getValueByColumnKey(groupConfigs[0])], dc[dHelper.getValueByColumnKey(aggregateConfigs[0])],]
+            });
+
+            Highcharts.chart(containerId, {
+                chart: {
+                    type: 'pyramid',
+                    marginRight: 100
+                },
+                title: {
+                    text: title,
+                    x: -50
+                },
+                plotOptions: {
+                    series: {
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b> ({point.y:,.0f})',
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                            softConnector: true
+                        }
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                series: [
+                    {
+                        name: desc,
+                        data: data
+                    }]
+            });
+
+        },
         onUnMount() { },
         onResize(options, context) { }
     }
