cc3ee54c0245ed97598557c193d3cc68fcc7db51
cui <devcui@outlook.com>
2022-09-26 16:18:34 +0800

    feat(highcharts): add over column basic chart

--- highcharts-column-basic-chart.js
+++ highcharts-column-basic-chart.js
@@ -2,7 +2,7 @@
  * @Author: cui<devcui@outlook.com>
  * @LastEditors: cui<devcui@outlook.com>
  * @Date: 2022-09-22 17:01:15
- * @LastEditTime: 2022-09-22 17:27:00
+ * @LastEditTime: 2022-09-26 16:17:24
  * @FilePath: \custom-chart-plugins\highcharts-column-basic-chart.js
  * @Description: 
  * 
@@ -17,7 +17,18 @@ function HighChartsColumnBasicChart({ dHelper }) {
                 { label: "dimension", key: "dimension", type: "group", allowSameField: false },
                 { label: "metrics", key: "metrics", type: "aggregate", allowSameField: false }
             ],
-            styles: [],
+            styles: [
+                {
+                    label: '标题',
+                    key: 'title',
+                    comType: 'input',
+                },
+                {
+                    label: '描述',
+                    key: 'desc',
+                    comType: 'input',
+                }
+            ],
             i18ns: [],
         },
         isISOContainer: 'highcharts',
@@ -94,7 +105,84 @@ function HighChartsColumnBasicChart({ dHelper }) {
                 }]
             });
         },
-        onUpdated(options, context) { },
+        onUpdated(options, context) {
+            if (!options.containerId || !context.document) return;
+            const { config, dataset, containerId } = options;
+            if (!dataset || !dataset.rows || dataset.rows.length === 0) return
+
+            const { window: { Highcharts } } = context
+            const styleConfigs = config.styles;
+
+            // 标题
+            let title = styleConfigs.filter((style) => style.key === 'title')
+            title = title.length > 0 ? title[0].value : 'title'
+
+            // 描述
+            let desc = styleConfigs.filter((style) => style.key === 'desc')
+            desc = desc.length > 0 ? desc[0].value : 'desc'
+
+            // 获取类型
+            const categories = dataset.rows.map(d => d[0])
+            const seriesNames = dataset.rows.map(d => d[1])
+
+            // 去重 
+            let filteredCategories = []
+            categories.forEach((c) => {
+                if (!filteredCategories.includes(c)) filteredCategories.push(c)
+            })
+            let filteredSeriesNames = []
+            seriesNames.forEach((c) => {
+                if (!filteredSeriesNames.includes(c)) filteredSeriesNames.push(c)
+            })
+
+            // 构造指标
+            let series = []
+            filteredSeriesNames.forEach((s) => {
+                const columnSerie = {
+                    name: s,
+                    data: [],
+
+                }
+                dataset.rows.forEach((r) => {
+                    if (r[1] === s) {
+                        columnSerie.data.push(r[2])
+                    }
+                })
+                series.push(columnSerie)
+            })
+            instance = Highcharts.chart(containerId, {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: title
+                },
+                subtitle: {
+                    text: desc
+                },
+                xAxis: {
+                    categories: filteredCategories,
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        borderWidth: 0
+                    }
+                },
+                series: series
+            });
+        },
         onUnMount() { },
         onResize(options, context) { }
     }
