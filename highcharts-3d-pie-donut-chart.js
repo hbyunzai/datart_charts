5c9837301776583212b6ecec84f326df8a55da5a
cui <devcui@outlook.com>
2022-09-26 17:26:37 +0800

    feat(highcharts): over pie

--- highcharts-3d-pie-donut-chart.js
+++ highcharts-3d-pie-donut-chart.js
@@ -2,8 +2,8 @@
  * @Author: cui<devcui@outlook.com>
  * @LastEditors: cui<devcui@outlook.com>
  * @Date: 2022-09-22 15:45:50
- * @LastEditTime: 2022-09-22 16:24:09
- * @FilePath: \custom-chart-plugins\highcharts-3d-donut-pie-chart.js
+ * @LastEditTime: 2022-09-26 17:25:57
+ * @FilePath: \custom-chart-plugins\highcharts-3d-pie-donut-chart.js
  * @Description: 
  * 
  * Copyright (c) 2022 by cui<devcui@outlook.com>, All Rights Reserved. 
@@ -18,7 +18,23 @@ function HighCharts3dPieChart({ dHelper }) {
                 { label: "dimension", key: "dimension", type: "group", allowSameField: false },
                 { label: "metrics", key: "metrics", type: "aggregate", allowSameField: false }
             ],
-            styles: [],
+            styles: [
+                {
+                    label: '标题',
+                    key: 'title',
+                    comType: 'input',
+                },
+                {
+                    label: '描述',
+                    key: 'desc',
+                    comType: 'input',
+                },
+                {
+                    label: '图例',
+                    key: 'name',
+                    comType: 'input',
+                }
+            ],
             i18ns: [],
         },
         isISOContainer: 'highcharts-3d',
@@ -80,7 +96,48 @@ function HighCharts3dPieChart({ dHelper }) {
                 }]
             });
         },
-        onUpdated(options, context) { },
+        onUpdated(options, context) {
+            if (!options.containerId || !context.document) return;
+            const { config, dataset, containerId } = options;
+            if (!dataset || !dataset.rows || dataset.rows.length === 0) return
+            const { window: { Highcharts } } = context
+            const styleConfigs = config.styles;
+            // 标题
+            let title = styleConfigs.filter((style) => style.key === 'title')
+            title = title.length > 0 ? title[0].value : 'title'
+            // 描述
+            let desc = styleConfigs.filter((style) => style.key === 'desc')
+            desc = desc.length > 0 ? desc[0].value : 'desc'
+            // 图例
+            let name = styleConfigs.filter((style) => style.key === 'name')
+            name = name.length > 0 ? name[0].value : 'name'
+            instance = Highcharts.chart(containerId, {
+                chart: {
+                    type: 'pie',
+                    options3d: {
+                        enabled: true,
+                        alpha: 45
+                    }
+                },
+                title: {
+                    text: title
+                },
+                subtitle: {
+                    text: desc
+                },
+                plotOptions: {
+                    pie: {
+                        innerSize: 100,
+                        depth: 45
+                    }
+                },
+                series: [{
+                    name: name,
+                    data: dataset.rows
+                }]
+            });
+
+        },
         onUnMount() { },
         onResize(options, context) { }
     }
